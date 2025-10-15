7353ebab7c03e92a951a648152d65251b409a534
cordy <mglgain@gmail.com>
2025-10-16 05:19:38 +0800

    updates: Version 0.7.1 refactor: Configurations

--- client__provider.tsx
+++ client__provider.tsx
@@ -1,12 +1,19 @@
 'use client';
 import { ReactNode } from 'react';
 import dynamic from 'next/dynamic';
+import type { Config } from 'wagmi';
 
 // Dynamically import WalletProviders with SSR disabled
 const WalletProviders = dynamic(() => import('./wagmi__providers'), {
   ssr: false,
 });
 
-export default function ProvidersClientWrapper({ children }: { children: ReactNode }) {
-  return <WalletProviders>{children}</WalletProviders>;
+export default function ProvidersClientWrapper({ 
+  children,
+  config 
+}: { 
+  children: ReactNode;
+  config?: Config;
+}) {
+  return <WalletProviders config={config}>{children}</WalletProviders>;
 }
--- dist/client__provider.d.ts
+++ dist/client__provider.d.ts
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
-export default function ProvidersClientWrapper({ children }: {
+import type { Config } from 'wagmi';
+export default function ProvidersClientWrapper({ children, config }: {
     children: ReactNode;
+    config?: Config;
 }): import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=client__provider.d.ts.map
