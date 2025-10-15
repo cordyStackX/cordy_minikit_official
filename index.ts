adb212a8bc06ecc838e33f468d5bd7efee4fe477
cordy <mglgain@gmail.com>
2025-10-16 01:50:41 +0800

    updates: Version 0.4.5 fix: modify Viem Custom chains support

--- index.ts
+++ index.ts
@@ -2,9 +2,8 @@
 // Clients
 export { default as ProvidersClientWrapper } from "./client__provider";
 
-//
+// Config
 export { default as getConfig } from "./config/walletConfig";
-
 
 //Components
 export * from "./components";
