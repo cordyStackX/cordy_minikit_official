27465c6387fe756a64cf180b5a0e089ccfa05d4c
cordy <mglgain@gmail.com>
2025-10-17 02:54:44 +0800

    Update: Versions 1.4.3 fix: Remove Automatic Network Enforcement

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,6 @@ export { default as ProvidersClientWrapper } from "./client__provider";
 export { default as getConfig } from "./config/walletConfig";
 // Hooks
 export { useWalletModal } from "./wagmi__providers";
-export { useNetworkGuard } from "./hooks/useNetworkGuard";
-// Utils
-export { detectContractNetwork, enforceNetwork, setupNetworkGuard, getNetworkByChainId, isNetworkSupported, addCustomNetwork, addCustomNetworks, createNetworkConfig, NETWORKS } from "./utils/networkEnforcer";
 //Components
 export * from "./components";
 //Controllers
