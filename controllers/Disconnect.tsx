be5c9cad56dd5db6ad3c3ed3a12146b2f4224227
cordy <mglgain@gmail.com>
2025-10-16 08:01:52 +0800

    updates: Version 0.9.0

--- controllers/Disconnect.tsx
+++ controllers/Disconnect.tsx
@@ -3,6 +3,8 @@ import { useDisconnect } from 'wagmi';
 export default function Disconnect() {
   const { disconnect } = useDisconnect();
 
+  window.location.reload();
+
   return (
      <button onClick={() => disconnect()}>Disconnect</button>
   );
